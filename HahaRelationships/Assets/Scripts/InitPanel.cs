03a2fba12374ce0b16a63c16cc6badba7f4c59f1
CyberHydrate <1905491799@qq.com>
2026-04-30 18:30:16 +0800

    游戏初始界面和GameManager框架

---
+++ HahaRelationships/Assets/Scripts/InitPanel.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitPanel : MonoBehaviour
+{
+    //这个脚本负责在游戏状态切换时显示或隐藏初始界面
+    [Header("初始界面")]
+    public GameObject content;
+    private void OnEnable()
+    {
+    }
+    private void OnDisable()
+    {
+        //取消订阅，避免内存泄漏
+        GameManager.Instance.OnGameStateChanged -= OnStateChanged;
+    }
+    void Start()
+    {
+        //初始订阅游戏状态改变事件
+        GameManager.Instance.OnGameStateChanged += OnStateChanged;
+        //根据当前状态设置初始界面显示
+        OnStateChanged(GameManager.Instance.CurrentState); 
+    }
+    void OnStateChanged(GameState state)
+    {
+        content.SetActive(state == GameState.MainMenu);
+    }
+}
